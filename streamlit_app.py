89355047891c537f928a500888b803ec1814528e
yosoymanojreddydve <forawspurpose@gmail.com>
2024-12-27 20:24:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ INGREDIENTS_LIST = st.multiselect('Choose upto 5 ingredients:',dataframe,max_sel
 if INGREDIENTS_LIST:
     ingredients_string = ''
     for fruit_chosen in INGREDIENTS_LIST:
-        ingredients_string+=fruit_chosen + '   '
+        ingredients_string+=fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
