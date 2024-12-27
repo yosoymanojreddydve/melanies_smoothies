92624e9f7e417e405872335fd982240b95d4261a
yosoymanojreddydve <forawspurpose@gmail.com>
2024-12-27 17:36:03 +0530

    Update streamlit_app.py
    adding the changes for running it in original steamlit app with making snowflake connection through code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,6 +9,8 @@ st.write(
     """
 )
 
+cnx = st.connect('snowflake')
+session = cnx.session()
 
 name_on_order = st.text_input("Name on Smoothie : ")
 st.write("The Name on your smoothie will be:", name_on_order)
