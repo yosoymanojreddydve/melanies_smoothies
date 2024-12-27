d2ec7831271432a62031f1a34cf7b066373581ec
yosoymanojreddydve <forawspurpose@gmail.com>
2024-12-27 19:49:32 +0530

    Update streamlit_app.py
    adding the search on value so that it will retreive the data using the search on value in the API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ if INGREDIENTS_LIST:
     for fruit_chosen in INGREDIENTS_LIST:
         ingredients_string+=fruit_chosen + '   '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         # st.text(smoothiefroot_response)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         # st.write(ingredients_string)
